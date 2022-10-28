d08bab27dcb880ba55972d3a7078c1c3fdcafb30
Jay Herron <NeedleInAJayStack@protonmail.com>
2022-10-28 00:23:19 -0600

    test: Adds JSON testing to String and Bool

--- Tests/HaystackTests/BoolTests.swift
+++ Tests/HaystackTests/BoolTests.swift
@@ -2,6 +2,38 @@ import XCTest
 @testable import Haystack
 
 final class BoolTests: XCTestCase {
+    func testJsonEncode() throws {
+        let encoder = JSONEncoder()
+        
+        let trueData = try encoder.encode(true)
+        XCTAssertEqual(
+            String(data: trueData, encoding: .utf8),
+            "true"
+        )
+        
+        let falseData = try encoder.encode(false)
+        XCTAssertEqual(
+            String(data: falseData, encoding: .utf8),
+            "false"
+        )
+    }
+    
+    func testJsonDecode() throws {
+        let decoder = JSONDecoder()
+        
+        let trueData = try XCTUnwrap("true".data(using: .utf8))
+        XCTAssertEqual(
+            try decoder.decode(Bool.self, from: trueData),
+            true
+        )
+        
+        let falseData = try XCTUnwrap("false".data(using: .utf8))
+        XCTAssertEqual(
+            try decoder.decode(Bool.self, from: falseData),
+            false
+        )
+    }
+    
     func testToZinc() throws {
         XCTAssertEqual(false.toZinc(), "F")
         XCTAssertEqual(true.toZinc(), "T")
--- Tests/HaystackTests/StringTests.swift
+++ Tests/HaystackTests/StringTests.swift
@@ -2,6 +2,26 @@ import XCTest
 @testable import Haystack
 
 final class StringTests: XCTestCase {
+    func testJsonEncode() throws {
+        let encoder = JSONEncoder()
+        
+        let stringData = try encoder.encode("hello")
+        XCTAssertEqual(
+            String(data: stringData, encoding: .utf8),
+            #""hello""#
+        )
+    }
+    
+    func testJsonDecode() throws {
+        let decoder = JSONDecoder()
+        
+        let stringData = try XCTUnwrap(#""hello""#.data(using: .utf8))
+        XCTAssertEqual(
+            try decoder.decode(String.self, from: stringData),
+            "hello"
+        )
+    }
+    
     func testToZinc() throws {
         XCTAssertEqual("hello".toZinc(), #""hello""#)
         XCTAssertEqual("_ \\ \" \n \r \t \u{0011} _".toZinc(), #""_ \\ \" \n \r \t \u0011 _""#)
