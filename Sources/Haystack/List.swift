b42cd474271c777b9ef2a5fa822911d2bf43c915
Jay Herron <NeedleInAJayStack@protonmail.com>
2022-10-30 23:11:30 -0600

    feature: Adds List type

---
+++ Sources/Haystack/List.swift
@@ -0,0 +1,76 @@
+import Foundation
+
+struct List: Val {
+    public var valType: ValType { .List }
+    
+    var elements: [any Val]
+    
+    public init(_ elements: [any Val]) {
+        self.elements = elements
+    }
+}
+
+/// See https://project-haystack.org/doc/docHaystack/Json#list
+extension List: Encodable {
+    public init(from decoder: Decoder) throws {
+        guard var container = try? decoder.unkeyedContainer() else {
+            throw DecodingError.typeMismatch(
+                Self.self,
+                .init(
+                    codingPath: [],
+                    debugDescription: "List representation must be an array"
+                )
+            )
+        }
+        
+        elements = []
+        containerLoop: while !container.isAtEnd {
+            typeLoop: for type in ValType.allCases {
+                if let val = try? container.decode(type.type) {
+                    elements.append(val)
+                    break typeLoop
+                }
+            }
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.unkeyedContainer()
+        for element in elements {
+            try container.encode(element)
+        }
+    }
+}
+
+// List + Equatable
+extension List {
+    static func == (lhs: List, rhs: List) -> Bool {
+        guard lhs.elements.count == rhs.elements.count else {
+            return false
+        }
+        
+        for (lhsElement, rhsElement) in zip(lhs.elements, rhs.elements) {
+            guard lhsElement.equals(rhsElement) else {
+                return false
+            }
+        }
+        
+        return true
+    }
+}
+
+// List + Hashable
+extension List {
+    func hash(into hasher: inout Hasher) {
+        for element in elements {
+            hasher.combine(element)
+        }
+    }
+}
+
+// List + ExpressibleByDictionaryLiteral
+extension List: ExpressibleByArrayLiteral {
+    init(arrayLiteral: any Val...) {
+        self.elements = arrayLiteral
+    }
+}
---
+++ Tests/HaystackTests/ListTests.swift
@@ -0,0 +1,115 @@
+import XCTest
+@testable import Haystack
+
+final class ListTests: XCTestCase {
+    func testJsonCoding() throws {
+        let value: List = [
+            true,
+            "abc",
+            Number(val: 42, unit: "furloghs"),
+            List([
+                false,
+                "xyz"
+            ])
+        ]
+        let jsonString = #"[true,"abc",{"_kind":"number","val":42,"unit":"furloghs"},[false,"xyz"]]"#
+        
+        let encodedData = try JSONEncoder().encode(value)
+        XCTAssertEqual(
+            String(data: encodedData, encoding: .utf8),
+            jsonString
+        )
+        
+        let decodedData = try XCTUnwrap(jsonString.data(using: .utf8))
+        XCTAssertEqual(
+            try JSONDecoder().decode(List.self, from: decodedData),
+            value
+        )
+    }
+    
+    func testEquatable() {
+        // Test basic
+        XCTAssertEqual (
+            List(["a"]),
+            List(["a"])
+        )
+        XCTAssertNotEqual (
+            List(["a"]),
+            List(["b"])
+        )
+        
+        // Test element count matters
+        XCTAssertNotEqual (
+            List([
+                "a",
+                "a",
+            ]),
+            List([
+                "a",
+            ])
+        )
+        XCTAssertNotEqual (
+            List([
+                "a",
+            ]),
+            List([
+                "a",
+                "a",
+            ])
+        )
+        
+        // Test order matters
+        XCTAssertNotEqual (
+            List([
+                "a",
+                "b",
+            ]),
+            List([
+                "b",
+                "a",
+            ])
+        )
+        
+        // Test nested
+        XCTAssertEqual (
+            List([
+                true,
+                "abc",
+                Number(val: 42, unit: "furloghs"),
+                List([
+                    false,
+                    "xyz"
+                ])
+            ]),
+            List([
+                true,
+                "abc",
+                Number(val: 42, unit: "furloghs"),
+                List([
+                    false,
+                    "xyz"
+                ])
+            ])
+        )
+        XCTAssertNotEqual (
+            List([
+                true,
+                "abc",
+                Number(val: 42, unit: "furloghs"),
+                List([
+                    false,
+                    "xyz"
+                ])
+            ]),
+            List([
+                true,
+                "abc",
+                Number(val: 42, unit: "furloghs"),
+                List([
+                    true,
+                    "xyz"
+                ])
+            ])
+        )
+    }
+}
