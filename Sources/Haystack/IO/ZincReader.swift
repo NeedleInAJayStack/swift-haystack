f08a3a6d6ec647da19b84f1a1d20edcfdd263eb5
Jay Herron <NeedleInAJayStack@protonmail.com>
2022-11-13 19:01:35 -0700

    feature: Adds ZincReader and tests

---
+++ Sources/Haystack/IO/ZincReader.swift
@@ -0,0 +1,311 @@
+import Foundation
+
+public class ZincReader {
+    let tokenizer: ZincTokenizer
+    
+    var cur: ZincToken = .eof
+    var curVal: any Val = null
+    var curLine: Int = 0
+    var peek: ZincToken = .eof
+    var peekVal: any Val = null
+    var peekLine: Int = 0
+    
+    
+    public init(_ zinc: String) throws {
+        tokenizer = ZincTokenizer(zinc)
+        try consume()
+        try consume()
+    }
+    
+    public func readVal() throws -> any Val {
+        var val: any Val = null
+        if cur == .id, curVal.equals("ver") {
+            val = try parseGrid()
+        } else {
+            val = try parseVal()
+        }
+        try verify(.eof)
+        return val
+    }
+    
+    public func readGrid() throws -> Grid {
+        guard let grid = try readVal() as? Grid else {
+            throw ZincReaderError.InputIsNotGrid
+        }
+        return grid
+    }
+    
+    private func parseVal() throws -> any Val {
+        if cur == .id {
+            guard let id = curVal as? String else {
+                throw ZincReaderError.IdValueIsNotString(curVal)
+            }
+            try consume(.id)
+            
+            // check for Coord or XStr
+            if cur == .lparen {
+                if peek == .num {
+                    return try parseCoord(id)
+                } else {
+                    return try parseXStr(id)
+                }
+            }
+            
+            switch id {
+            case "T": return true
+            case "F": return false
+            case "N": return null
+            case "M": return marker
+            case "R": return remove
+            case "NA": return na
+            case "NaN": return Number(val: .nan)
+            case "INF": return Number(val: .infinity)
+            default:
+                throw ZincReaderError.UnexpectedId(id)
+            }
+        }
+        
+        if cur.isLiteral {
+            return try parseLiteral()
+        }
+        
+        // -INF
+        if cur == .minus, peek == .id, peekVal.equals("INF") {
+            try consume(.minus)
+            try consume(.id)
+            return Number(val: -1.0 * .infinity)
+        }
+        
+        if cur == .lbracket {
+            return try parseList()
+        }
+        if cur == .lbrace {
+            return try parseDict()
+        }
+        if cur == .lt2 {
+            return try parseGrid()
+        }
+        
+        throw ZincReaderError.UnexpectedToken(cur)
+    }
+    
+    private func parseCoord(_ id: String) throws -> Coord {
+        guard id == "C" else {
+            throw ZincReaderError.InvalidCoord
+        }
+        try consume(.lparen)
+        let latitude = try consumeNumber()
+        try consume(.comma)
+        let longitude = try consumeNumber()
+        try consume(.rparen)
+        return Coord(lat: latitude.val, lng: longitude.val)
+    }
+    
+    private func parseXStr(_ id: String) throws -> XStr {
+        guard (id.first?.isLowercase ?? false) else {
+            throw ZincReaderError.InvalidXStr
+        }
+        try consume(.lparen)
+        let val = try consumeString()
+        try consume(.rparen)
+        return XStr(type: id, val: val)
+    }
+    
+    private func parseLiteral() throws -> any Val {
+        var val = self.curVal
+        if cur == .ref, peek == .str {
+            guard let refVal = curVal as? String, let dis = peekVal as? String else {
+                throw ZincReaderError.InvalidRef
+            }
+            val = Ref(val: refVal, dis: dis)
+            try consume(.ref)
+        }
+        try consume()
+        return val
+    }
+    
+    private func parseList() throws -> List {
+        var elements = [any Val]()
+        while cur != .rbracket, cur != .eof {
+            try elements.append(parseVal())
+            guard cur == .comma else {
+                break
+            }
+            try consume(.comma)
+        }
+        try consume(.rbracket)
+        return List(elements)
+    }
+    
+    private func parseDict() throws -> Dict {
+        var elements = [String: any Val]()
+        let hasBraces = cur == .lbrace
+        if hasBraces {
+            try consume(.lbrace)
+        }
+        while cur == .id {
+            let id = try consumeTagName()
+            var val: any Val = marker
+            if cur == .colon {
+                try consume(.colon)
+                val = try parseVal()
+            }
+            elements[id] = val
+        }
+        if hasBraces {
+            try consume(.rbrace)
+        }
+        
+        return Dict(elements)
+    }
+    
+    private func parseGrid() throws -> Grid {
+        let isNested = cur == .lt2
+        if isNested {
+            try consume(.lt2)
+            if cur == .nl {
+                try consume(.nl)
+            }
+        }
+        
+        // Check version
+        guard cur == .id, curVal.equals("ver") else {
+            throw ZincReaderError.GridDoesNotBeginWithVersion(curVal)
+        }
+        try consume()
+        try consume(.colon)
+        let version = try consumeString()
+        guard version == "3.0" else {
+            throw ZincReaderError.UnsupportedZincVersion(version)
+        }
+        
+        // Metadata
+        let builder = GridBuilder()
+        if cur == .id {
+            try builder.setMeta(parseDict().elements)
+        }
+        try consume(.nl)
+        
+        // Columns
+        var numCols = 0
+        while cur == .id {
+            numCols += 1
+            let name = try consumeTagName()
+            var colMeta: [String: any Val]? = nil
+            if cur == .id {
+                colMeta = try parseDict().elements
+            }
+            try builder.addCol(name: name, meta: colMeta)
+            
+            guard cur == .comma else {
+                break
+            }
+            try consume(.comma)
+        }
+        guard numCols > 0 else {
+            throw ZincReaderError.GridHasNoColumns
+        }
+        try consume(.nl)
+        
+        // Rows
+        while true {
+            if cur == .nl || cur == .eof || (isNested && cur == .gt2) {
+                break
+            }
+            
+            var cells = [any Val]()
+            for i in 0 ..< numCols {
+                if cur == .comma || cur == .nl || cur == .eof {
+                    cells.append(null)
+                } else {
+                    try cells.append(parseVal())
+                }
+                if i+1 < numCols {
+                    try consume(.comma)
+                }
+            }
+            try builder.addRow(cells)
+            
+            if cur == .eof || (isNested && cur == .gt2) {
+                break
+            }
+            try consume(.nl)
+        }
+        
+        if cur == .nl {
+            try consume(.nl)
+        }
+        if isNested {
+            try consume(.gt2)
+        }
+        return builder.toGrid()
+    }
+    
+    // MARK: Token Reads
+    
+    private func consumeTagName() throws -> String {
+        try verify(.id)
+        guard let id = curVal as? String else {
+            throw ZincReaderError.IdValueIsNotString(curVal)
+        }
+        guard (id.first?.isLowercase ?? false) else {
+            throw ZincReaderError.InvalidTagName
+        }
+        try consume(.id)
+        return id
+    }
+    
+    private func consumeNumber() throws -> Number {
+        try verify(.num)
+        guard let number = curVal as? Number else {
+            throw ZincReaderError.NumValueIsNotNumber(curVal)
+        }
+        try consume(.num)
+        return number
+    }
+    
+    private func consumeString() throws -> String {
+        try verify(.str)
+        guard let number = curVal as? String else {
+            throw ZincReaderError.StrValueIsNotString(curVal)
+        }
+        try consume(.str)
+        return number
+    }
+    
+    private func consume(_ expected: ZincToken? = nil) throws {
+        if let expected = expected {
+            try verify(expected)
+        }
+        cur = peek;
+        curVal = peekVal;
+        curLine = peekLine;
+
+        peek = try tokenizer.next();
+        peekVal = tokenizer.val;
+        peekLine = tokenizer.line;
+    }
+    
+    private func verify(_ expected: ZincToken) throws {
+        if cur != expected {
+            throw ZincReaderError.ExpectedToken(expected, not: cur)
+        }
+    }
+}
+
+enum ZincReaderError: Error {
+    case ExpectedToken(ZincToken, not: ZincToken)
+    case GridDoesNotBeginWithVersion(any Val)
+    case GridHasNoColumns
+    case IdValueIsNotString(any Val)
+    case InvalidCoord
+    case InvalidRef
+    case InvalidTagName
+    case InvalidXStr
+    case NumValueIsNotNumber(any Val)
+    case StrValueIsNotString(any Val)
+    case UnexpectedId(String)
+    case UnexpectedToken(ZincToken)
+    case InputIsNotGrid
+    case UnsupportedZincVersion(String)
+}
---
+++ Tests/HaystackTests/IO/ZincReaderTests.swift
@@ -0,0 +1,261 @@
+import XCTest
+import Haystack
+
+final class ZincReaderTests: XCTestCase {
+    func testNullGridMetaAndColMeta() throws {
+        try XCTAssertEqualZincGrid(
+            zinc: """
+                ver:"3.0" tag:N
+                a nullmetatag:N, b markermetatag
+                
+                """,
+            meta: ["tag": null],
+            cols: [("a", ["nullmetatag": null]), ("b", ["markermetatag": marker])],
+            rows: []
+        )
+    }
+    
+    func testGridWithSymbols() throws {
+        try XCTAssertEqualZincGrid(
+            zinc: """
+                ver:"3.0"
+                a,b
+                ^foo,^a-b
+                """,
+            meta: [:],
+            cols: [("a", nil), ("b", nil)],
+            rows: [
+                [Symbol(val: "foo"), Symbol(val: "a-b")]
+            ]
+        )
+    }
+    
+    func test() throws {
+        try XCTAssertEqualZincGrid(
+            zinc: """
+                ver:"3.0"
+                fooBar33
+                
+                """,
+            meta: [:],
+            cols: [("fooBar33", nil)],
+            rows: []
+        )
+        
+        try XCTAssertEqualZincGrid(
+            zinc: """
+                ver:"3.0" tag foo:"bar"
+                xyz
+                "val"
+                """,
+            meta: ["tag": marker, "foo": "bar"],
+            cols: [("xyz", nil)],
+            rows: [
+                ["val"]
+            ]
+        )
+        
+        try XCTAssertEqualZincGrid(
+            zinc: """
+                ver:"3.0"
+                val
+                N
+                """,
+            meta: [:],
+            cols: [
+                ("val", nil),
+            ],
+            rows: [
+                [null]
+            ]
+        )
+        
+        try XCTAssertEqualZincGrid(
+            zinc: """
+                ver:"3.0"
+                a,b
+                1,2
+                3,4
+                """,
+            meta: [:],
+            cols: [("a", nil), ("b", nil)],
+            rows: [
+                [Number(val: 1), Number(val: 2)],
+                [Number(val: 3), Number(val: 4)],
+            ]
+        )
+        
+        try XCTAssertEqualZincGrid(
+            zinc: """
+                ver:"3.0"
+                a,    b,      c,      d
+                T,    F,      N,   -99
+                2.3,  -5e-10, 2.4e20, 123e-10
+                "",   "a",   "\\" \\\\ \t \n \r", "\u{abcd}"
+                `path`, @12cbb082-0c02ae73, 4s, -2.5min
+                M,R,N,N
+                2009-12-31, 23:59:01, 01:02:03.123, 2009-02-03T04:05:06Z
+                INF, -INF, \"\", NaN
+                C(12,-34),C(0.123,-0.789),C(84.5,-77.45),C(-90,180)
+                """,
+            meta: [:],
+            cols: [("a", nil), ("b", nil), ("c", nil), ("d", nil)],
+            rows: [
+                [true, false, null, Number(val: -99)],
+                [Number(val: 2.3), Number(val: -5e-10), Number(val: 2.4e20), Number(val: 123e-10)],
+                ["", "a", "\" \\ \t \n \r", "\u{abcd}"],
+                [Uri(val: "path"), Ref(val: "12cbb082-0c02ae73"), Number(val: 4, unit: "s"), Number(val: -2.5, unit: "min")],
+                [marker, remove, null, null], // Bin not supported.
+                [Date(year: 2009, month: 12, day: 31), Time(hour: 23, minute: 59, second: 1), Time(hour: 1, minute: 2, second: 3, millisecond: 123), DateTime(year: 2009, month: 2, day: 3, hour: 4, minute: 5, second: 6)],
+                [Number(val: .infinity), Number(val: -1 * .infinity), "", Number(val: .nan)],
+                [Coord(lat: 12, lng: -34), Coord(lat: 0.123, lng: -0.789), Coord(lat: 84.5, lng: -77.45), Coord(lat: -90, lng: 180)],
+            ]
+        )
+        
+        try XCTAssertEqualZincGrid(
+            zinc: """
+                ver:"3.0"
+                foo
+                `foo$20bar`
+                `foo\\`bar`
+                `file \\#2`
+                "$15"
+                """,
+            meta: [:],
+            cols: [("foo", nil)],
+            rows: [
+                [Uri(val: "foo$20bar")],
+                [Uri(val: "foo`bar")],
+                [Uri(val: "file \\#2")],
+                ["$15"],
+            ]
+        )
+        
+        try XCTAssertEqualZincGrid(
+            zinc: """
+                ver:"3.0"
+                a,b
+                -3.1kg,4kg
+                5%,3.2%
+                5kWh/ft\u{00b2},-15kWh/m\u{00b2}
+                123e+12kJ/kg_dry,74\u{0394}\u{00b0F}
+                """,
+            meta: [:],
+            cols: [("a", nil), ("b", nil)],
+            rows: [
+                [Number(val: -3.1, unit: "kg"), Number(val: 4, unit: "kg")],
+                [Number(val: 5, unit: "%"), Number(val: 3.2, unit: "%")],
+                [Number(val: 5, unit: "kWh/ft\u{00b2}"), Number(val: -15, unit: "kWh/m\u{00b2}")],
+                [Number(val: 123e+12, unit: "kJ/kg_dry"), Number(val: 74, unit: "\u{0394}\u{00b0F}")],
+            ]
+        )
+        
+        try XCTAssertEqualZincGrid(
+            zinc: """
+                ver:"3.0"
+                a, b, c
+                , 1, 2
+                3, , 5
+                6, 7_000,
+                ,,10
+                ,,
+                14,,
+                """,
+            meta: [:],
+            cols: [("a", nil), ("b", nil), ("c", nil)],
+            rows: [
+                [null, Number(val: 1), Number(val: 2)],
+                [Number(val: 3), null, Number(val: 5)],
+                [Number(val: 6), Number(val: 7000), null],
+                [null, null, Number(val: 10)],
+                [null, null, null],
+                [Number(val: 14), null, null],
+            ]
+        )
+        
+        try XCTAssertEqualZincGrid(
+            zinc: """
+                ver:"3.0"
+                a,b
+                2010-03-01T23:55:00.013-05:00 GMT+5,2010-03-01T23:55:00.013+10:00 GMT-10
+                """,
+            meta: [:],
+            cols: [("a", nil), ("b", nil)],
+            rows: [
+                [
+                    DateTime(year: 2010, month: 3, day: 1, hour: 23, minute: 55, second: 0, millisecond: 13, gmtOffset: -5*60*60, timezone: "GMT+5"),
+                    DateTime(year: 2010, month: 3, day: 1, hour: 23, minute: 55, second: 0, millisecond: 13, gmtOffset: 10*60*60, timezone: "GMT-10"),
+                ],
+            ]
+        )
+        
+        try XCTAssertEqualZincGrid(
+            zinc: """
+                ver:"3.0" a: 2009-02-03T04:05:06Z foo b: 2010-02-03T04:05:06Z UTC bar c: 2009-12-03T04:05:06Z London baz
+                a
+                3.814697265625E-6
+                2010-12-18T14:11:30.924Z
+                2010-12-18T14:11:30.925Z UTC
+                2010-12-18T14:11:30.925Z London
+                45$
+                33\u{00a3}
+                @12cbb08e-0c02ae73
+                7.15625E-4kWh/ft\u{00b2}
+                R
+                NA
+                """,
+            meta: [
+                "a": DateTime(year: 2009, month: 2, day: 3, hour: 4, minute: 5, second: 6),
+                "foo": marker,
+                "b": DateTime(year: 2010, month: 2, day: 3, hour: 4, minute: 5, second: 6),
+                "bar": marker,
+                "c": DateTime(year: 2009, month: 12, day: 3, hour: 4, minute: 5, second: 6, timezone: "London"),
+                "baz": marker,
+            ],
+            cols: [("a", nil)],
+            rows: [
+                [Number(val: 3.814697265625E-6)],
+                [DateTime(year: 2010, month: 12, day: 18, hour: 14, minute: 11, second: 30, millisecond: 924)],
+                [DateTime(year: 2010, month: 12, day: 18, hour: 14, minute: 11, second: 30, millisecond: 925)],
+                [DateTime(year: 2010, month: 12, day: 18, hour: 14, minute: 11, second: 30, millisecond: 925, timezone: "London")],
+                [Number(val: 45, unit: "$")],
+                [Number(val: 33, unit: "\u{00a3}")],
+                [Ref(val: "12cbb08e-0c02ae73")],
+                [Number(val: 7.15625E-4, unit: "kWh/ft\u{00b2}")],
+                [remove],
+                [na],
+            ]
+        )
+    }
+    
+    private func XCTAssertEqualZincGrid(
+        zinc: String,
+        meta: [String: any Val],
+        cols: [(String, [String: any Val]?)],
+        rows: [[any Val]],
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) throws {
+        let actual = try ZincReader(zinc).readGrid()
+        
+        let builder = GridBuilder()
+        builder.setMeta(meta)
+        for (colName, colMeta) in cols {
+            try builder.addCol(name: colName, meta: colMeta)
+        }
+        for row in rows {
+            try builder.addRow(row)
+        }
+        let expected = builder.toGrid()
+        
+        XCTAssertEqual(
+            actual,
+            expected,
+            """
+            \(actual.toZinc())
+            is not equal to
+            \(expected.toZinc())
+            """
+        )
+    }
+}
